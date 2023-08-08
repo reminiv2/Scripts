6371f0e087696e849bfb280f96413af9b13ed851
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-08 15:17:34 -0400

    Create tag.lua

---
+++ tag.lua
@@ -0,0 +1,7 @@
+-- open-source
+-- use if you want idc
+repeat wait()
+if game.Players:FindFirstChild("modezero5000") then
+game.Players["modezero5000"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["modezero5000"].DisplayName)
+end
+until game.Players:FindFirstChild("modezero5000")
