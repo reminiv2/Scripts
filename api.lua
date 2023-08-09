d947c155bf8883527525bf84d407b518e260923a
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-08 20:22:26 -0400

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,8 +1,8 @@
 -- auto updater
 getgenv().api = false
-
 if getgenv().api == true then
 -- stats: working
 elseif getgenv().api == false then
 game.Players.LocalPlayer:Kick("script updating | or down")
 end
+
