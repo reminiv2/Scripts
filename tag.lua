0225babbb7749f22c19b5e44933fb5435de05853
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-08 20:14:16 -0400

    Update tag.lua

--- tag.lua
+++ tag.lua
@@ -2,6 +2,14 @@
 -- use if you want idc
 repeat wait()
 if game.Players:FindFirstChild("modezero5000") then
+if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
 game.Players["modezero5000"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["modezero5000"].DisplayName)
 end
+end
 until game.Players:FindFirstChild("modezero5000")
+if game.Players:FindFirstChild("modezero5000") then
+local Player = game.Players["modezero5000"]
+game.Players.PlayerAdded:connect(function(Player)
+game.Players["modezero5000"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["modezero5000"].DisplayName)
+end)
+end
