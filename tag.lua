d01ad6c3a1e203fa106858a8774e8ec24ec8fc41
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-11 05:41:46 -0400

    Update tag.lua

--- tag.lua
+++ tag.lua
@@ -1,15 +1,30 @@
 -- open-source
 -- use if you want idc
 repeat wait()
-if game.Players:FindFirstChild("modezero5000") then
+if game.Players:FindFirstChild("sp0zx") then
 if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
-game.Players["modezero5000"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["modezero5000"].DisplayName)
+game.Players["sp0zx"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["sp0zx"].DisplayName)
 end
 end
-until game.Players:FindFirstChild("modezero5000")
-if game.Players:FindFirstChild("modezero5000") then
-local Player = game.Players["modezero5000"]
+until game.Players:FindFirstChild("sp0zx")
+if game.Players:FindFirstChild("sp0zx") then
+local Player = game.Players["sp0zx"]
 game.Players.PlayerAdded:connect(function(Player)
-game.Players["modezero5000"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["modezero5000"].DisplayName)
+game.Players["sp0zx"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["sp0zx"].DisplayName)
+end)
+end
+
+
+repeat wait()
+if game.Players:FindFirstChild("iiiiiii1iiiii1111iii") then
+if game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+game.Players["iiiiiii1iiiii1111iii"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["iiiiiii1iiiii1111iii"].DisplayName)
+end
+end
+until game.Players:FindFirstChild("iiiiiii1iiiii1111iii")
+if game.Players:FindFirstChild("iiiiiii1iiiii1111iii") then
+local Player = game.Players["iiiiiii1iiiii1111iii"]
+game.Players.PlayerAdded:connect(function(Player)
+game.Players["iiiiiii1iiiii1111iii"].Character.Humanoid.DisplayName = ('[📜] ' .. game.Players["iiiiiii1iiiii1111iii"].DisplayName)
 end)
 end
