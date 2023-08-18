81f724eff82cf5dbb114cbb3d2be8376c3730bcf
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-18 12:13:28 -0400

    Update whilylis.lua

--- whilylis.lua
+++ whilylis.lua
@@ -10,7 +10,7 @@ local SDIKjFNIEJSMNFdiks = {
     "AE1B70D4-4E81-44AB-9DB9-6B8220ECD80B", -- 5.iz (buyer)
     "67A66F6F-BAB8-41A0-943E-71C9236F5CC9", -- blade (buyer)
     -- "D66B2B4B-E023-4811-A561-C8A9DE8469B7", -- ssaint (tester)
-    "20A421CB-27B7-4D5A-A697-600127B7A38B", -- vesper (buyer)
+    --"20A421CB-27B7-4D5A-A697-600127B7A38B", -- vesper (blacklisted, reason: sharing personal info)
     "936C4012-C0A4-4751-8F48-F5A096B6A13D" -- yaduvir (buyer)
 }
 if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
